f20b52f8c5d2460df6d60bfdcf57705d2da94ea8
Peurière <Peurire@192.168.1.42>
2016-12-11 14:49:40 +0100

    javadoc Soldat

--- src/wargame/Soldat.java
+++ src/wargame/Soldat.java
@@ -1,54 +1,92 @@
 package wargame;
-
+/**
+ * Classe des Soldats (Heros et Monstres)
+ */
 public abstract class Soldat extends Element implements ISoldat, IConfig
 {
 	private static final long serialVersionUID = -3756577513065161512L;
 	
+	/**
+	 * Points de vie du Soldat
+	 */
 	protected int points_de_vie;
+	/**
+	 * Portee visuelle (d'attaque) du soldat
+	 */
 	protected int portee_visuelle;
+	/**
+	 * Puissance (corps a corps) du soldat
+	 */
 	protected int puissance;
+	/**
+	 * Puissance de tir (distance) du soldat
+	 */
 	protected int tir;
 	
-	public java.awt.Color couleur;
-	
+	/**
+	 * Constructeur d'un Soldat
+	 * 
+	 * @param x Position horizontale sur la map
+	 * @param y Position verticale sur la map
+	 */
 	public Soldat(int x,int y)
 	{
 		super(x,y);
 		vide=false;
 	}
 	
-	/** Renvoi les points de vie */
+	/** 
+	 * Renvoie les points de vie du soldat
+	 * @return points_de_vie
+	 */
 	public int getPoints()
 	{
 		return points_de_vie;
 	}
-	
+	/**
+	 * Renvoie le Tour (non utilisé) du soldat
+	 */
 	public int getTour()
 	{
 		return this.getTour();
 	}
-	
+	/**
+	 * Renvoie la portée du soldat
+	 * @return portee_visuelle
+	 */
 	public int getPortee()
 	{
 		return portee_visuelle;
 	}
-	
+	/**
+	 * Renvoie la puissance du soldat
+	 * @return puissance
+	 */
 	public int getPuissance()
 	{
 		return puissance;
 	}
-	
+	/**
+	 * Renvoie la puissance de tir du soldat
+	 * @return tir
+	 */
 	public int getTir()
 	{
 		return tir;
 	}
-	
+	/**
+	 * Joue le tour
+	 * (Non utilisé)
+	 */
 	public void joueTour(int tour)
 	{
 		
 	}
-	
-	/** Renvoi vrai si le combat est gagné par le soldat courant */
+	/**
+	 * 	Methode de combat en le soldat actuel et celui passé en parametre
+	 * @param soldat le soldat attaqué
+	 * @return vrai si le combat est gagné par le soldat courant
+	 */
 	public boolean combat(Soldat soldat)
 	{
 		if (this.getPosition().estVoisine(soldat.getPosition()))	//A coté de l'ennemi (8 cases)
@@ -67,7 +105,10 @@ public abstract class Soldat extends Element implements ISoldat, IConfig
 		
 		return (getPoints() > 0 && soldat.getPoints() <= 0);	//Vrai si combat gagné
 	}
-	
+	/**
+	 * Deplace le soldat a la nouvelle position
+	 * @param newPos Nouvelle position de deplacement
+	 */
 	public void seDeplace(Position newPos)
 	{
 		this.setPosition(newPos);
